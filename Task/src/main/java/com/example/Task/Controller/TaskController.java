63e2726946e76e39f32eb7412ef2f86205fb3a49
Geethamadhuri16 <84759801+Geethamadhuri16@users.noreply.github.com>
2024-01-18 22:15:55 +0530

    Add files via upload

---
+++ Task/src/main/java/com/example/Task/Controller/TaskController.java
@@ -0,0 +1,54 @@
+package com.example.Task.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Task.DTO.TaskDTO;
+import com.example.Task.Model.Task;
+import com.example.Task.Service.TaskService;
+
+@RestController
+public class TaskController {
+	
+	@Autowired
+	private TaskService service;
+	
+	@PostMapping("/createTask")
+	public ResponseEntity<Task> createTask(@RequestBody Task task){
+		return service.addTask(task);
+		
+		
+	}
+	
+	@PutMapping("/assign/{id}")
+	public ResponseEntity<String> updateTask(@PathVariable("id") Long id,@RequestBody TaskDTO  dto){
+		return service.updateTask(id, dto);
+		
+	}
+	
+	@GetMapping("/status/{id}")
+	public ResponseEntity<String> checkStatus(@PathVariable("id")Long id){
+		return service.checkStatus(id);
+	}
+	
+	@GetMapping("/DueDate/{id}")
+	public ResponseEntity<String> dueDate(@PathVariable("id")Long id){
+		return service.checkDueDate(id);
+	}
+	
+	@GetMapping("/checkByEmail/{mail}")
+	public ResponseEntity<List<Task>> getBymail(@PathVariable("mail")String mail){
+		return service.getByEmail(mail);
+	}
+	
+
+}
