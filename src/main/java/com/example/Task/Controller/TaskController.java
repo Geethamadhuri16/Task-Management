5952c0b39f97ee26540882130cf6e170aabf6efd
Geetha Madhuri <84759801+Geethamadhuri16@users.noreply.github.com>
2024-06-28 21:38:18 +0530

    Update TaskController.java

--- src/main/java/com/example/Task/Controller/TaskController.java
+++ src/main/java/com/example/Task/Controller/TaskController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.Task.DTO.TaskDTO;
 import com.example.Task.Model.Task;
 import com.example.Task.Service.TaskService;
-
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/task")
 public class TaskController {
