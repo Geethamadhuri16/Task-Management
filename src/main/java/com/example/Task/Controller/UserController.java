b04197ebc2f02f2912573fe9549d3ce8fe6e2028
Geetha Madhuri <84759801+Geethamadhuri16@users.noreply.github.com>
2024-06-28 21:45:35 +0530

    Update UserController.java

--- src/main/java/com/example/Task/Controller/UserController.java
+++ src/main/java/com/example/Task/Controller/UserController.java
@@ -1,5 +1,32 @@
 package com.example.Task.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.Task.Model.User;
+import com.example.Task.Service.UserService;
 
 public class UserController {
+  @Autowired
+	    private UserService userService;
+
+	    @PostMapping("/register")
+	    public ResponseEntity<String> registerUser(@RequestBody User user) {
+	        return userService.register(user);
+	    }
+
+	    @PostMapping("/login")
+	    public ResponseEntity<String> loginUser(@RequestParam String mail, @RequestParam String pass) {
+	        return userService.login(mail, pass);
+	    }
+
+	    @DeleteMapping("/deleteUser")
+	    public ResponseEntity<String> deleteUser(@RequestParam Long id) {
+	        return userService.del(id);
+	    }
+
 
 }
